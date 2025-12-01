61548a70eefe27b4b5f5f34e5febb45f6ad34e94
ryanJackman05 <20108256@mail.wit.ie>
2025-12-01 12:48:46 +0000

    web server done i think

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+
+const filePath = "instanceID.txt";
+instanceID = "bleh"
+
+// Read → Modify → Write
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) throw err;
+
+  instanceID = data;
+});
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Home Page" });
+  res.render("index", { title: "Home Page", instID: instanceID});
 });
 
 router.get("/about", (req, res) => {
